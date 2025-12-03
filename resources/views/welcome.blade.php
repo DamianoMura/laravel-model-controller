5f28d7bd580933256a716d5d460c5db0effe6d11
DamianoMura <mura.damiano14@gmail.com>
2025-12-03 14:38:07 +0100

    [upd] enhance exercise instructions with tooltip for additional context

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -28,15 +28,21 @@
         <p>Oggi facciamo la nostra prima vera interazione con il database utilizzando Eloquent, l’ORM di Laravel!</p>
         <img src="{{ Vite::asset('./resources/img/screen.png')}}" alt="screenshot" class="w-250 mx-auto my-5">
         <h3 class="text-3xl py-3">SVOLGIMENTO</h3>
-        <ol class="text-center list-decimal list-inside max-w-lg mx-auto">
-            <li>Create un nuovo progetto Laravel</li> 
-            <li>tramite phpMyAdmin o MySQL Workbench create un nuovo database laravel_model_controller</li>
-            <li>Importate nel vostro database la tabella movies in allegato</li>
-            <li>inserite le vostre credenziali per il database nel file .env</li>
+        <ol class="ex-list  text-left list-decimal list-inside max-w-lg mx-auto">
+           
+            <li >Create un nuovo progetto Laravel</li> 
+            <li class="group-1">- Tramite phpMyAdmin o MySQL Workbench create un nuovo database laravel_model_controller 
+            Importate nel vostro database la tabella movies in allegato
+            inserite le vostre credenziali per il database nel file .env
+                <span class="subtext-1"> questa versione di Laravel crea un database se non esiste, quindi a patto che le credenziali
+                siano valide nel file .env, questo passaggio viene eseguito con il comando "php artisan migrate" oppure "php artisan
+                migrate:fresh" per droppare e ri creare da zero il database</span>
+            </li>
             <li>Create un model Movie.php col comando php artisan make:model Movie</li>
             <li>Create un controller che gestirà la rotta / col comando php artisan make:controller Guest/PageController</li>
             <li>All’interno della funzione index() del controller, recuperate tutti i film dal database e passateli alla view, che
             quindi li visualizzerà a schermo, tramite delle card.</li>
+           
         </ol>
         <h4 class="text-3xl py-3">BONUS</h4>
         <p>
