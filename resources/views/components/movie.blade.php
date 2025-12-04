54d84fd16750d111736df2b7d16186f273e33e1c
DamianoMura <mura.damiano14@gmail.com>
2025-12-04 13:15:45 +0100

    [upd] basic styling

---
+++ resources/views/components/movie.blade.php
@@ -0,0 +1,5 @@
+<div class="bg-blue-200 p-4 rounded shadow">
+    {{$slot}}
+    
+    
+</div>
--- resources/views/movies.blade.php
+++ resources/views/movies.blade.php
@@ -1,17 +1,16 @@
 @extends('layouts.default')
 @section('content')
-<div class="container mx-auto my-5 text-center">
-  <ul>
-    @foreach ($movies as $movie)
-    <li>
-        <h2 class="text-5xl font-semibold">{{ $movie->title }}</h2>
-        <h3 class="text-2xl font-semibold">{{ $movie->original_title }}</h3>
-        <h4>{{$movie->nationality}}</h4>
-        <p class="text-gray-600">Release Year: {{ $movie->date }}</p>
-    </li>
-      
-    @endforeach
-
-  </ul>
-</div>
+  <div class="container mx-auto px-4 py-8 text-center">
+    <h1 class="text-3xl font-bold mb-6">Movies List</h1>
+      <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-5 gap-4">
+        @foreach ($movies as $movie)
+        <x-movie>
+          <h2 class="text-xl font-semibold">{{ $movie->title }}</h2>
+          <p class="text-gray-700 font-bold">( {{ $movie->original_title }} ) </p>
+          <p class="text-gray-700">Release date: {{ $movie->date }}</p>
+          <p class="text-gray-700">vote: {{ $movie->vote }}</p>
+        </x-movie>
+        @endforeach
+      </div>
+  </div>
 @endsection
