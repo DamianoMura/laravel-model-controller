3cd3c028e6bfdfdd595b4c47116e048502b6ae9b
DamianoMura <mura.damiano14@gmail.com>
2025-12-04 13:19:58 +0100

    [add] missing link to movies page

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,8 +2,8 @@
 @section('content')
 <div class="container mx-auto my-5 text-center">
     <h1 class="text-4xl font-bold mb-4">Welcome to the Movie Database</h1>
-    <p class="text-lg text-gray-700">Explore our collection of movies and find your next favorite!</p>    
-    
+    <p class="text-lg text-gray-700 mb-5">Explore our collection of movies and find your next favorite!</p>    
+    <a href="/movies" class="px-4 py-2 bg-blue-700 text-white rounded-2xl"> visualizza</a>
     
 </div>
 @endsection
